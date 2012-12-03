c56c6103412bdbfae96f90713e796e268751ffc0
dokmatik <no_spam@gmail.com>
2012-12-03 21:49:28 +0100

    first stable version

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,11 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('InfoDashboard', ['InfoDashboard.filters', 'InfoDashboard.services', 'InfoDashboard.directives']).
+var dashBoardApp = angular.module('InfoDashboard', ['InfoDashboard.filters', 'InfoDashboard.services', 'InfoDashboard.directives','ngResource']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
+
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,57 @@
 
 /* Controllers */
 
+var dashBoardApp = angular.module('InfoDashboard')
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+dashBoardApp.directive('fadein', function () {
+    return {
+        restrict:'A',
+        link:function (scope, elm, attrs) {
+            /*jQuery(elm)
+             .css({ opacity: 0 })
+             .animate({ opacity: 1 }, parseInt(attrs.fadein));
+             */
+            elm.bind("click", function () {
+                alert("yeah")
+            })
+            jQuery(elm).fadeIn(parseInt(attrs.fadein) + scope.$index * 250)
+            /*
+             jQuery(elm).next(".fullTweet").show("slow", function() {
+             jQuery(this).next(".fullTweet").fadeIn("50",arguments.callee)
+             });*/
+        }
+    };
+})
+var twitterClient = {
+    url:"http://search.twitter.com/search.json?callback=?",
+    url_nocallback:"http://search.twitter.com/search.json"
+}
+
+function MyCtrl1($scope, $http, $resource) {
+    $scope.tweets = {}
+    $scope.twitter = $resource('http://search.twitter.com/:action',
+        {action:'search.json',
+            callback:'JSON_CALLBACK',
+            rpp:10,
+            randomParam:Math.random() * 1000}, {
+            get : {method : 'JSONP'}
+        })
+
+    $scope.doSearch = function () {
+        $scope.tweets = $scope.twitter.get({q: $scope.searchQuery})
+    }
+    $scope.previousTweets = function () {
+        if (this.tweets.previous_page) {
+            $scope.tweets = $scope.twitter.get({page : parseInt($scope.tweets.page)-1, q: $scope.searchQuery})
+        }
+    }
+    $scope.nextTweets = function () {
+        if (this.tweets.next_page) {
+            $scope.tweets = $scope.twitter.get({page : parseInt($scope.tweets.page)+1, q: $scope.searchQuery})
+        }
+    }
+}
+//MyCtrl1.$inject = [];
 
 
 function MyCtrl2() {
