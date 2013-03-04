ce3c39a962f05fce58f15653af5e472cc6208171
dokmatik <no_spam@gmail.com>
2013-03-04 14:12:59 +0100

    more angular magic

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-var dashBoardApp = angular.module('InfoDashboard', ['InfoDashboard.filters', 'InfoDashboard.services', 'InfoDashboard.directives','ngResource']).
+var dashBoardApp = angular.module('InfoDashboard', ['InfoDashboard.filters', 'InfoDashboard.services', 'InfoDashboard.directives','ngResource',]).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: SearchController});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,50 +6,89 @@ var dashBoardApp = angular.module('InfoDashboard')
 
 dashBoardApp.directive('fadein', function () {
     return {
-        restrict:'A',
-        link:function (scope, elm, attrs) {
-            elm.css("display","none");
+        restrict : 'A',
+        link : function (scope, elm, attrs) {
+            elm.css("display", "none");
             if (scope.$last) {
-                jQuery(".fullTweet:eq(0)").show("slow", function() {
-                    jQuery(this).next(".fullTweet").fadeIn(parseInt(attrs.fadein),arguments.callee)
+                jQuery("ol>.fullTweet:eq(0)").show("slow", function () {
+                    jQuery(this).next(".fullTweet").fadeIn(parseInt(attrs.fadein), arguments.callee)
                 });
             }
         }
     };
 })
+
+dashBoardApp.factory('mySharedService', function($rootScope) {
+    return {
+        broadcast: function(msg) {
+            $rootScope.$broadcast('handleBroadcast', msg); 
+        },
+		reset : function(msg) {
+			$rootScope.$broadcast('reset');
+		}
+    };
+});
+
 var twitterClient = {
     url:"http://search.twitter.com/search.json?callback=?",
     url_nocallback:"http://search.twitter.com/search.json"
 }
 
-
-function MyCtrl1($scope, $http, $resource, $timeout) {
-    $scope.tweets = {}
-    $scope.twitter = $resource('http://search.twitter.com/:action',
-        {action:'search.json',
-            callback:'JSON_CALLBACK',
-            rpp:10,
-            randomParam:Math.random() * 1000}, {
-            get : {method : 'JSONP'}
-        })
-
+function SearchController($scope, sharedService) {
     $scope.doSearch = function () {
-        $scope.tweets = $scope.twitter.get({q: $scope.searchQuery})
+		sharedService.reset();
+       sharedService.broadcast($scope.searchQuery)
     }
+}
+SearchController.$inject=['$scope','mySharedService']
+
+function MyCtrl1($scope, $http, $resource, $timeout, sharedService, twitter) {
+    $scope.tweets = {}
+
+	$scope.$on('handleBroadcast',function(event, msg) {
+		$scope.tweets = twitter.get({q:msg});
+	});
+	
     $scope.previousTweets = function () {
         if (this.tweets.previous_page) {
-            $scope.tweets = $scope.twitter.get({page : parseInt($scope.tweets.page)-1, q: $scope.searchQuery})
+            $scope.tweets = twitter.paginate({page : parseInt($scope.tweets.page)-1, q: $scope.searchQuery})
         }
     }
     $scope.nextTweets = function () {
         if (this.tweets.next_page) {
-            $scope.tweets = $scope.twitter.get({page : parseInt($scope.tweets.page)+1, q: $scope.searchQuery})
+            $scope.tweets = twitter.paginate({page : parseInt($scope.tweets.page)+1, q: $scope.searchQuery})
         }
     }
 }
-//MyCtrl1.$inject = [];
+MyCtrl1.$inject = ['$scope','$http','$resource','$timeout','mySharedService','twitter'];
 
 
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function BooksController($scope, $http, $resource, $timeout, sharedService, googleBooks) {
+	$scope.books = {};
+	$scope.startIndex=0;
+
+	$scope.$on('handleBroadcast', function(event, msg) {
+		$scope.books = googleBooks.volumes({q : msg, startIndex : $scope.startIndex*10}, function(data) {
+			$scope.fills = [];
+			$scope.fills.length=10-data.items.length
+		});
+	});
+	$scope.$on('reset', function(event) {
+		$scope.startIndex=0;
+	})
+	
+	$scope.previous=function() {
+		$scope.startIndex--;
+		$scope.$broadcast('handleBroadcast', $scope.$parent.searchQuery);
+	}
+	$scope.next=function() {
+		$scope.startIndex++;
+		$scope.$broadcast('handleBroadcast', $scope.$parent.searchQuery);
+	}
+	
+}
+BooksController.$inject = ['$scope','$http','$resource','$timeout','mySharedService', 'googleBooks'];
